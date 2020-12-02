58ea0ffdb15c3450d7dc817d80139d40ed68684f
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-12-02 02:35:52 +0100

    🎉 Initial commit

---
+++ src/IDay.cs
@@ -0,0 +1,7 @@
+namespace AdventOfCode2020
+{
+    public interface IDay
+    {
+        public string Calculate();
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day = 0;
+            try
+            {
+                day = Int32.Parse(args[0]);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Select Day!");
+                return;
+            }
+
+            var type = Type.GetType($"Day{day}");
+
+            if (type is null)
+            {
+                Console.WriteLine($"Day{day} solution does not exist!");
+                return;
+            }
+
+            if (Activator.CreateInstance(type) is not IDay solution)
+            {
+                Console.WriteLine($"Day{day} solution does not exist!");
+                return;
+            }
+
+            solution.Calculate();
+        }
+    }
+}
